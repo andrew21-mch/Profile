ef4f76bf4068fe92c8cee4b24fd66deeb68f977c
andrew21-mch <nfonandrew73@gmail.com>
2022-01-04 13:29:42 +0100

    updated

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,6 +1,6 @@
 /**
 * PHP Email Form Validation - v3.2
-* URL: https://bootstrapmade.com/php-email-form/
+* URL: https://bootstr apmade.com/php-email-form/
 * Author: BootstrapMade.com
 */
 (function () {
